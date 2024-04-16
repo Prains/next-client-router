0e06634b8c64a0a84d03a840f4de229ad0a2d9a6
prains <sleskov2013@yandex.ru>
2024-04-16 15:50:12 +0300

    fix

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-export { default as clientRouter } from "./src/clientRouter";
+const clientRouter = {
+  push: (url: string) => window.history.pushState(undefined, "", url),
+  replace: (url: string) => window.history.replaceState(undefined, "", url),
+};
+
+export default clientRouter;
--- src/clientRouter.ts
+++
@@ -1,7 +0,0 @@
-const clientRouter = {
-    push: (url: string) => window.history.pushState(undefined, "", url),
-    replace: (url: string) => window.history.replaceState(undefined, "", url),
-  };
-  
-  export default clientRouter;
-  
